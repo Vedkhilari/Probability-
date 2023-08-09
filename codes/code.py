7d8d7c494c56174ccc7877fc2a14a8eec82ec943
Vedkhilari <ee22btech11056@gmail.com>
2023-08-09 12:34:51 +0530

    Second commit

--- codes/code.py
+++ codes/code.py
@@ -1,9 +1,5 @@
 import numpy as np
 import math 
-x1=float(input("enter first component of first vector"))
-y1=float(input("enter second component of first vector"))
-x2=float(input("enter first component of second vector"))
-y2=float(input("enter second component of second vector"))
-vector=np.array([x2-x1,y2-y1])
+vector=np.array([float(input("enter first component of first vector"))-float(input("enter first component of second vector")),float(input("enter second component of first vector"))-float(input("enter second component of second vector"))])
 magnitude=np.linalg.norm(vector)
 print("Magnitude=", magnitude)
